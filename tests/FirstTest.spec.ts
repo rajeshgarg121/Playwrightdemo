572e65fecb77bc895053728a8a7b1f4c77645d45
Rajesh <rajeshgarg121@gmail.com>
2026-09-04 20:45:15 +0530

    updated user name and password

--- tests/FirstTest.spec.ts
+++ tests/FirstTest.spec.ts
@@ -5,7 +5,7 @@ test('has title',{tag: '@smoke'}, async ({page}) => {
   const username = process.env.PLAYWRIGHT_USER;
   await page.goto('https://playwright.dev/');  
   console.log("user name is "+username);
-  await expect(page).toHaveTitle(/junk/);
+  await expect(page).toHaveTitle(/playwright/);
 });
 
 test('get started link', async ({page}) => {
@@ -13,3 +13,15 @@ test('get started link', async ({page}) => {
   await page.getByRole('link', {name: 'Get started'}).click();  
   await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
 });
+
+test  ('check login', {tag: '@smoke2'}, async ({page}) => {
+  const username = process.env.PLAYWRIGHT_USER;
+  const password = process.env.PLAYWRIGHT_PASSWORD;
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
+  await page.getByLabel('Username').fill(username);``
+  await page.getByLabel('Password').fill(password);
+  console.log("user name is "+username);  
+  console.log("password is "+password);
+  await page.getByRole('button', {name: 'Submit'}).click();
+  await expect(page.getByRole('heading', {name: 'Logged in successfully'})).toBeVisible();
+})
