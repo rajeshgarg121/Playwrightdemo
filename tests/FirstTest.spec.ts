3c2b3697ef4c3e9787940af3b59d827e76951c8b
Rajesh <rajeshgarg121@gmail.com>
2026-09-04 15:13:54 +0530

    config and test title change

--- tests/FirstTest.spec.ts
+++ tests/FirstTest.spec.ts
@@ -2,7 +2,7 @@ import {test,expect} from '@playwright/test';
 
 test('has title',{tag: '@smoke'}, async ({page}) => {
   await page.goto('https://playwright.dev/');  
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/junk/);
 });
 
 test('get started link', async ({page}) => {
