61876eeb07cbbebb649394804daabb8f3e701510
Rajesh <rajeshgarg121@gmail.com>
2026-09-04 12:33:05 +0530

    first commit

---
+++ tests/FirstTest.spec.ts
@@ -0,0 +1,12 @@
+import {test,expect} from '@playwright/test';
+
+test('has title',{tag: '@smoke'}, async ({page}) => {
+  await page.goto('https://playwright.dev/');  
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({page}) => {
+  await page.goto('https://playwright.dev/');  
+  await page.getByRole('link', {name: 'Get started'}).click();  
+  await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
+});
